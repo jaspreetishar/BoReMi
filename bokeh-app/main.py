e8d3b1f295799e2e8a667f1541bd2405d0649d10
Jaspreet Ishar <103258471+jaspreetishar@users.noreply.github.com>
2023-04-02 17:21:43 +0300

    changes

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -13,7 +13,7 @@ import Functions_FOR_BINDER as BINDER
 # In[9]:
 
 
-file_names = ["/home/jovyan/Binder/Sample Input/Spatio-molecular data/subsampled_mf_obs.csv"]
+file_names = ["/Sample Input/Spatio-molecular data/subsampled_mf_obs.csv"]
 #file_names = [r"C:\Users\NPS\Downloads\MAT_1_DATA_TYPE\mf_obs.csv"]
 x_coordinate_column_names = ["center_x"]
 y_coordinate_column_names = ["center_y"]
@@ -35,10 +35,10 @@ title = ["MERFISH"]
 
 # Addresses of the "HE image" files along with their pixel size.
 
-imgs = ["/home/jovyan/Binder/Sample Input/HE images/1.jpg", 
-        "/home/jovyan/Binder/Sample Input/HE images/2.jpg", 
-        "/home/jovyan/Binder/Sample Input/HE images/3.jpg", 
-        "/home/jovyan/Binder/Sample Input/HE images/4.jpg"]
+imgs = ["/Sample Input/HE images/1.jpg", 
+        "/Sample Input/HE images/2.jpg", 
+        "/Sample Input/HE images/3.jpg", 
+        "/Sample Input/HE images/4.jpg"]
 
 scale_for_he_image = 0.14
 
